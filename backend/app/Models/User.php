693d769732c5908de26bd9de01a86a17340db242
unknown <salet2015@outlook.com>
2022-04-12 22:45:29 +0200

    izmena user modela

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $primaryKey = 'id';
+    
     /**
      * The attributes that are mass assignable.
      *
@@ -21,6 +23,9 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'username',
+        'api_token',
+        'role'
     ];
 
     /**
