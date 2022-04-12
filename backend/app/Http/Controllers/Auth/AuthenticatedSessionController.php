34ec26dd3d21976129d7a2cf989ffab68e8650ed
unknown <salet2015@outlook.com>
2022-04-12 22:43:28 +0200

    izmenjeni kontroleri za handlovanje autentifikacije

--- backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -17,11 +19,37 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request)
     {
-        $request->authenticate();
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
 
-        $request->session()->regenerate();
+        // $credentials['password'] = Hash::make($credentials['password']);
 
-        return response()->noContent();
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return response()->json(['success' => true, 'user' => Auth::user()]);
+        }
+
+        return response()->json(['success' => false, 'message' => 'The provided credentials do not match our records.']);
+    }
+
+    public function logout(Request $request)
+    {
+        $token = $request->api_token;
+
+        $authenticatedUser = User::where('api_token', $token)->first();
+        if($authenticatedUser && $authenticatedUser->id === Auth::user()->id) 
+        {
+            Auth::logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+
+            return response()->json(['success' => true]);
+        }
+
+        return response()->json(['success' => false]);
     }
 
     /**
--- backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\Rules;
+use Illuminate\Support\Str;
 
 class RegisteredUserController extends Controller
 {
@@ -26,18 +27,25 @@ class RegisteredUserController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'username' => ['required', 'string', 'max:15'],
+            'role' => ['required', 'integer', 'min:1', 'max:3'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
+            'username' => $request->username,
             'password' => Hash::make($request->password),
+            'api_token' => Str::random(60),
+            'role' => $request->role,
         ]);
 
-        event(new Registered($user));
+        return response()->json(['success' => true, 'user' => $user]);
 
-        Auth::login($user);
+        // event(new Registered($user));
 
-        return response()->noContent();
+        // Auth::login($user);
+
+        // return response()->noContent();
     }
 }
