4d8209bf9ef252c4cbcb55758275c2cdfe4e6445
unknown <salet2015@outlook.com>
2022-04-12 22:41:07 +0200

    izmenjena tabela korisnika

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,10 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('lastname');
             $table->string('email')->unique();
             $table->string('username')->unique();
             $table->string('password');
+            // $table->string('api_token')->unique()->nullable()->default(null);
             $table->integer('role');
             $table->timestamps();
         });
