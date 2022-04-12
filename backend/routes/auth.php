4825e855860899e53275dd2fdef70eda72fd11cf
unknown <salet2015@outlook.com>
2022-04-12 22:40:17 +0200

    dodata ruta za logout

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -32,6 +32,5 @@ Route::post('/email/verification-notification', [EmailVerificationNotificationCo
                 ->middleware(['auth', 'throttle:6,1'])
                 ->name('verification.send');
 
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-                ->middleware('auth')
+Route::post('/logout', [AuthenticatedSessionController::class, 'logout'])
                 ->name('logout');
