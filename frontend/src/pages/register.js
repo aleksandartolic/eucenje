a73f28878cefcb52d0d08c8ad1b0a5ed4f5537b3
nmarkovic <pomocnih@gmail.com>
2022-05-07 13:28:55 +0200

    Add register page

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -1,66 +1,100 @@
-import ApplicationLogo from '@/components/ApplicationLogo'
-import AuthCard from '@/components/AuthCard'
 import AuthValidationErrors from '@/components/AuthValidationErrors'
-import Button from '@/components/Button'
-import GuestLayout from '@/components/Layouts/GuestLayout'
-import Input from '@/components/Input'
-import Label from '@/components/Label'
-import Link from 'next/link'
+
 import { useAuth } from '@/hooks/auth'
 import { useState } from 'react'
+import image from '../assets/images/backgroundLogin.jpg'
+import FormWrapper from '@/components/FormWrapper'
+import {
+    TextField,
+    Typography,
+    Button,
+    MenuItem,
+    Select,
+    FormControl,
+    InputLabel,
+} from '@mui/material'
+import styled from 'styled-components'
+import Link from 'next/link'
 
 const Register = () => {
-    const { register } = useAuth({
-        redirectIfAuthenticated: '/dashboard',
-    })
-
+    const { register } = useAuth()
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [password_confirmation, setPasswordConfirmation] = useState('')
+    const [role, setRole] = useState('')
+    const [username, setUsername] = useState('')
     const [errors, setErrors] = useState([])
 
-    const submitForm = event => {
+    const submitForm = async event => {
         event.preventDefault()
-
-        register({ name, email, password, password_confirmation, setErrors })
+        register({
+            name,
+            email,
+            password,
+            password_confirmation,
+            username,
+            role,
+            setErrors,
+        })
     }
 
     return (
-        <GuestLayout>
-            <AuthCard
-                logo={
-                    <Link href="/">
-                        <a>
-                            <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                        </a>
-                    </Link>
-                }>
-
-                {/* Validation Errors */}
-                <AuthValidationErrors className="mb-4" errors={errors} />
-
-                <form onSubmit={submitForm}>
-                    {/* Name */}
-                    <div>
-                        <Label htmlFor="name">Name</Label>
-
-                        <Input
+        <LoginLayout image={image}>
+            <LoginWrapper>
+                <FormWrapper>
+                    <Typography
+                        style={{
+                            marginBottom: '3rem',
+                            textAlign: 'center',
+                            color: '#6495ED',
+                        }}
+                        variant="h3">
+                        A d e m y
+                    </Typography>
+                    <AuthValidationErrors
+                        style={{ marginBottom: '20px' }}
+                        errors={errors}
+                    />
+                    <form
+                        style={{ width: '100%', height: '100%' }}
+                        onSubmit={submitForm}>
+                        <TextField
+                            inputProps={{ style: { fontSize: 17 } }} // font size of input text
+                            InputLabelProps={{ style: { fontSize: 13 } }}
+                            style={{
+                                width: '100%',
+                                margin: '5px',
+                                marginBottom: '2rem',
+                            }}
+                            style={{
+                                width: '100%',
+                                margin: '5px',
+                            }}
+                            label="Name"
+                            variant="outlined"
                             id="name"
-                            type="text"
+                            type="name"
                             value={name}
                             className="block mt-1 w-full"
                             onChange={event => setName(event.target.value)}
                             required
-                            autoFocus
                         />
-                    </div>
-
-                    {/* Email Address */}
-                    <div className="mt-4">
-                        <Label htmlFor="email">Email</Label>
-
-                        <Input
+                        <br />
+                        <TextField
+                            inputProps={{ style: { fontSize: 17 } }} // font size of input text
+                            InputLabelProps={{ style: { fontSize: 13 } }}
+                            style={{
+                                width: '100%',
+                                margin: '5px',
+                                marginBottom: '2rem',
+                            }}
+                            style={{
+                                width: '100%',
+                                margin: '5px',
+                            }}
+                            label="Email"
+                            variant="outlined"
                             id="email"
                             type="email"
                             value={email}
@@ -68,13 +102,43 @@ const Register = () => {
                             onChange={event => setEmail(event.target.value)}
                             required
                         />
-                    </div>
-
-                    {/* Password */}
-                    <div className="mt-4">
-                        <Label htmlFor="password">Password</Label>
-
-                        <Input
+                        <br />
+                        <TextField
+                            inputProps={{ style: { fontSize: 17 } }} // font size of input text
+                            InputLabelProps={{ style: { fontSize: 13 } }}
+                            style={{
+                                width: '100%',
+                                margin: '5px',
+                                marginBottom: '2rem',
+                            }}
+                            style={{
+                                width: '100%',
+                                margin: '5px',
+                            }}
+                            label="Username"
+                            variant="outlined"
+                            id="username"
+                            type="text"
+                            value={username}
+                            className="block mt-1 w-full"
+                            onChange={event => setUsername(event.target.value)}
+                            required
+                        />
+                        <br />
+                        <TextField
+                            inputProps={{ style: { fontSize: 17 } }} // font size of input text
+                            InputLabelProps={{ style: { fontSize: 13 } }}
+                            style={{
+                                width: '100%',
+                                margin: '5px',
+                                marginBottom: '2rem',
+                            }}
+                            style={{
+                                width: '100%',
+                                margin: '5px',
+                            }}
+                            label="Password"
+                            variant="outlined"
                             id="password"
                             type="password"
                             value={password}
@@ -83,15 +147,17 @@ const Register = () => {
                             required
                             autoComplete="new-password"
                         />
-                    </div>
-
-                    {/* Confirm Password */}
-                    <div className="mt-4">
-                        <Label htmlFor="password_confirmation">
-                            Confirm Password
-                        </Label>
-
-                        <Input
+                        <br />
+                        <TextField
+                            inputProps={{ style: { fontSize: 17 } }} // font size of input text
+                            InputLabelProps={{ style: { fontSize: 13 } }}
+                            style={{
+                                width: '100%',
+                                margin: '5px',
+                                marginBottom: '2rem',
+                            }}
+                            label="Confirm Password"
+                            variant="outlined"
                             id="password_confirmation"
                             type="password"
                             value={password_confirmation}
@@ -101,21 +167,85 @@ const Register = () => {
                             }
                             required
                         />
-                    </div>
-
-                    <div className="flex items-center justify-end mt-4">
-                        <Link href="/login">
-                            <a className="underline text-sm text-gray-600 hover:text-gray-900">
-                                Already registered?
-                            </a>
-                        </Link>
-
-                        <Button className="ml-4">Register</Button>
-                    </div>
-                </form>
-            </AuthCard>
-        </GuestLayout>
+                        <br />
+                        <FormControl
+                            style={{
+                                width: '100%',
+                                margin: '0 5px 10px 5px',
+                            }}>
+                            <InputLabel
+                                style={{
+                                    width: '100%',
+                                    marginTop: '0px',
+                                    fontSize: '13px',
+                                }}
+                                htmlFor="role">
+                                Role
+                            </InputLabel>
+                            <Select
+                                label="Role"
+                                variant="outlined"
+                                id="role"
+                                type="text"
+                                value={role}
+                                className="block mt-1 w-full"
+                                onChange={event => setRole(event.target.value)}
+                                required>
+                                <MenuItem value={1}>Polaznik</MenuItem>
+                                <MenuItem value={2}>Predavac</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <br />
+                        <Typography variant="h5">
+                            {' '}
+                            You already have an account ?{' '}
+                            <Link prefetch href="/login">
+                                <StyledLink href="/login">Sign In</StyledLink>
+                            </Link>
+                        </Typography>
+                        <br />
+                        <Button
+                            className="ml-4"
+                            style={{
+                                marginTop: '1rem',
+                                fontSize: 10,
+                                width: '90px',
+                                height: '30px',
+                            }}
+                            type="submit"
+                            variant="contained"
+                            color="primary">
+                            register
+                        </Button>
+                    </form>
+                </FormWrapper>
+            </LoginWrapper>
+        </LoginLayout>
     )
 }
 
 export default Register
+
+const LoginWrapper = styled.div`
+    position: absolute;
+    left: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`
+
+const LoginLayout = styled.div`
+    background: url(${props => props.image.src});
+    background-size: cover;
+    width: 100%;
+    height: 100vh;
+`
+
+const StyledLink = styled.a`
+    text-decoration: none;
+    color: #93b5f2;
+    :visited {
+        color: #93b5f2;
+    }
+`
