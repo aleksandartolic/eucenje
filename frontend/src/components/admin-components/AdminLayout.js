16ab792404739f3e580b8edda653fb7fcc1f15e1
nmarkovic <pomocnih@gmail.com>
2022-05-10 22:13:09 +0200

    Add admin layout

---
+++ frontend/src/components/admin-components/AdminLayout.js
@@ -0,0 +1,11 @@
+import NavBar from '@/components/admin-components/NavBar'
+import { Fragment } from 'react'
+
+const AdminLayout = () => {
+    return (
+        <Fragment>
+            <NavBar />
+        </Fragment>
+    )
+}
+export default AdminLayout
