4a258eccf9d3c9f456d79daa6b960487f40b0f37
nmarkovic <pomocnih@gmail.com>
2022-05-11 23:02:36 +0200

    Fix admin layout

--- frontend/src/components/admin-components/AdminLayout.js
+++ frontend/src/components/admin-components/AdminLayout.js
@@ -1,10 +1,19 @@
 import NavBar from '@/components/admin-components/NavBar'
 import { Fragment } from 'react'
+import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Footer from '@/components/admin-components/Footer'
+import * as React from 'react'
 
-const AdminLayout = () => {
+const AdminLayout = props => {
     return (
         <Fragment>
-            <NavBar />
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <NavBar />
+                {props.children}
+            </Box>
+            <Footer />
         </Fragment>
     )
 }
